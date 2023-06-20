af720d423158aa6d6f8a08254b497e9204a6e3bb
JessePomeroy <thinkingofview@gmail.com>
2023-06-20 17:10:22 -0500

    issues

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
-const { v4: uuidv4 } = require('uuid');
 const { clog } = require('./middleware/clog');
 
 const PORT = process.env.PORT || 3000;
